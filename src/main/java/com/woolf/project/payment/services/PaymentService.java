52e5b735d9b2b841c57e0d6f70a2bae63e0d948e
shubhammore <shubhammore1796@gmail.com>
2025-03-29 16:47:16 +0530

    RazorPay Callback Integration and Exception Handling

--- src/main/java/com/woolf/project/payment/services/PaymentService.java
+++ src/main/java/com/woolf/project/payment/services/PaymentService.java
@@ -3,14 +3,28 @@ package com.woolf.project.payment.services;
 import com.razorpay.PaymentLink;
 import com.razorpay.RazorpayClient;
 import com.razorpay.RazorpayException;
+import com.razorpay.Utils;
+import com.woolf.project.payment.dtos.PaymentCallbackRequestDTO;
+import com.woolf.project.payment.dtos.PaymentDTO;
 import com.woolf.project.payment.exceptions.NotFoundException;
 import com.woolf.project.payment.models.PaymentModel;
 import com.woolf.project.payment.repositories.PaymentRepository;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentService {
+
+    @Value("${razorpay.key_id}")
+    private String razorpayKeyId;
+
+    @Value("${razorpay.key_secret}")
+    private String razorpayKeySecret;
+
+    @Value("${razorpay.callback_url}")
+    private String razorpayCallbackUrl;
+
     private RazorpayClient razorpayClient;
     private PaymentRepository paymentRepository;
 
@@ -19,7 +33,7 @@ public class PaymentService {
         this.paymentRepository = paymentRepository;
     }
 
-    public String createPaymentOrder(Double amount, String currency, String invoiceNumber) throws RazorpayException {
+    public PaymentDTO createPaymentLink(Double amount, String currency, String invoiceNumber) throws RazorpayException {
         //We need to create checkout page to do it using order
         //        JSONObject orderRequest = new JSONObject();
 //        orderRequest.put("amount", amount * 100); // Amount in paise
@@ -31,12 +45,12 @@ public class PaymentService {
 
         // Do it using payment link
         JSONObject paymentLinkRequest = new JSONObject();
-        paymentLinkRequest.put("amount", amount * 100); // Amount in paise (e.g., 1000 = ₹10)
+        paymentLinkRequest.put("amount", amount*100); // Amount in paise (e.g., 1000 = ₹10)
         paymentLinkRequest.put("currency", currency);
         paymentLinkRequest.put("accept_partial", false);
         paymentLinkRequest.put("reference_id", invoiceNumber); // Unique reference ID (e.g., order ID)
-        paymentLinkRequest.put("callback_url", "http://localhost:4000/payments/callback"); // Callback URL
-        paymentLinkRequest.put("callback_method", "get"); // Callback method (GET or POST)
+        paymentLinkRequest.put("callback_url", razorpayCallbackUrl); // Callback URL
+        paymentLinkRequest.put("callback_method", "get");
 
         PaymentLink paymentLink = razorpayClient.paymentLink.create(paymentLinkRequest);
         System.out.println("Order created: " + paymentLink.toString());
@@ -50,19 +64,39 @@ public class PaymentService {
         payment.setStatus(paymentLink.get("status"));
 
         paymentRepository.save(payment);
-        return paymentLink.get("short_url");
+        return PaymentDTO.from(payment,paymentLink.get("short_url"));
     }
 
-    public String getPaymentStatus(String orderId) throws RazorpayException, NotFoundException {
-        PaymentModel payment = paymentRepository.findByOrderId(orderId)
-                .orElseThrow(() -> new NotFoundException("Payment not found for Order Id " + orderId));
-        return payment.getStatus();
+    public PaymentModel getPaymentDetails(String orderId) throws NotFoundException {
+        return paymentRepository.findByOrderId(orderId)
+                .orElseThrow(() -> new NotFoundException("Payment not found for orderId: "+orderId));
     }
 
-    public void fetchPaymentStatus(String paymentId) throws RazorpayException, NotFoundException {
-
+    public String fetchPaymentStatus_Razorpay(String paymentId) throws RazorpayException, NotFoundException {
+        return razorpayClient.payments.fetch(paymentId).toString();
     }
 
+    public boolean verifySignature(PaymentCallbackRequestDTO request)
+            throws RazorpayException {
+
+        JSONObject attributes = new JSONObject();
+        attributes.put("razorpay_signature",request.getRazorpay_signature());
+        attributes.put("payment_link_status",request.getRazorpay_payment_link_status());
+        attributes.put("payment_link_id",request.getRazorpay_payment_link_id());
+        attributes.put("payment_link_reference_id", request.getRazorpay_payment_link_reference_id());
+        attributes.put("razorpay_payment_id",request.getRazorpay_payment_id());
+        return Utils.verifyPaymentLink(attributes, razorpayKeySecret);
+    }
+
+    public void updatePaymentDetails(PaymentCallbackRequestDTO request) throws NotFoundException {
+        PaymentModel payment = paymentRepository.findByOrderId(request.getRazorpay_payment_link_id())
+                .orElseThrow(()-> new NotFoundException("Payment not found for payment link id:"
+                        +request.getRazorpay_payment_link_id()));
+
+        payment.setPaymentId(request.getRazorpay_payment_id());
+        payment.setStatus(request.getRazorpay_payment_link_status());
+        paymentRepository.save(payment);
+    }
 
 }
 
